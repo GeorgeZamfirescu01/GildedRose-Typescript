5aaf079aaa6a0b4d03785e5fc68da11044a98004
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-04 17:12:58 +0300

    Mooore refactoring
    Maybe I should stop. Does Softwire have any coding style adopted for Typescript?

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -13,74 +13,80 @@ export class Item {
 export class GildedRose {
     MAX_QUALITY = 50;
     MIN_QUALITY = 0;
+
     items: Array<Item>;
 
-    maxBound = val => {
+    limitBelowMax = (val: number) => {
         return Math.min(this.MAX_QUALITY, val);
     }
-    minBound = val => {
+    limitAboveMin = (val: number) => {
         return Math.max(this.MIN_QUALITY, val);
     }
 
     rules = {
-        'Aged Brie': item => {
+        'Aged Brie': (item: Item) => {
             const sellIn = item.sellIn;
             const quality = item.quality;
+
             const newSellIn = item.sellIn - 1;
             const newQuality =
                 quality >= this.MAX_QUALITY ?
                 quality :
-                this.maxBound(sellIn <= 0 ? quality + 2 : quality + 1);
+                this.limitBelowMax(sellIn <= 0 ? quality + 2 : quality + 1);
 
             return {newSellIn, newQuality};
         },
-        'Sulfuras, Hand of Ragnaros': item => {
+        'Sulfuras, Hand of Ragnaros': (item: Item) => {
             const sellIn = item.sellIn;
             const quality = item.quality;
+
             return {newSellIn: sellIn, newQuality: quality};
         },
-        'Backstage passes to a TAFKAL80ETC concert': item => {
+        'Backstage passes to a TAFKAL80ETC concert': (item: Item) => {
             const sellIn = item.sellIn;
             const quality = item.quality;
+
             const newSellIn = sellIn - 1;
             const newQuality = [
                 [sellIn <= 0, 0],
                 [sellIn <= 5,
                     quality >= this.MAX_QUALITY ?
                     quality :
-                    this.maxBound(quality + 3)],
+                    this.limitBelowMax(quality + 3)],
                 [sellIn <= 10,
                     quality >= this.MAX_QUALITY ?
                     quality :
-                    this.maxBound(quality + 2)],
+                    this.limitBelowMax(quality + 2)],
                 [true,
                     quality >= this.MAX_QUALITY ?
                     quality :
-                    this.maxBound(quality + 1)],
-            ].find(pair => pair[0])?.[1];
+                    this.limitBelowMax(quality + 1)],
+            ].find(boolValPair => boolValPair[0])?.[1];
 
             return {newSellIn, newQuality};
         },
-        'Conjured Mana Cake': item => {
+        'Conjured Mana Cake': (item: Item) => {
             const sellIn = item.sellIn;
             const quality = item.quality;
+
             const newSellIn = sellIn - 1;
             const newQuality =
                 quality < 0 ?
                 quality :
-                this.minBound(sellIn <= 0 ? quality - 4 : quality - 2);
+                this.limitAboveMin(sellIn <= 0 ? quality - 4 : quality - 2);
 
             return {newSellIn, newQuality};
         },
     }
-    defaultRule = item => {
+    defaultRule = (item: Item) => {
         const sellIn = item.sellIn;
         const quality = item.quality;
+
         const newSellIn = sellIn - 1;
         const newQuality =
             quality < 0 ?
             quality :
-            this.minBound(sellIn <= 0 ? quality - 2 : quality - 1);
+            this.limitAboveMin(sellIn <= 0 ? quality - 2 : quality - 1);
 
         return {newSellIn, newQuality};
     }
@@ -90,9 +96,7 @@ export class GildedRose {
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            const item = this.items[i];
-
+        this.items.forEach(item => {
             let rule = this.defaultRule;
             if (this.rules.hasOwnProperty(item.name)) {
                 rule = this.rules[item.name];
@@ -101,7 +105,7 @@ export class GildedRose {
             const {newSellIn, newQuality} = rule(item)
             item.sellIn = newSellIn;
             item.quality = newQuality;
-        }
+        });
 
         return this.items;
     }
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -6,9 +6,9 @@ describe('Gilded Rose', function () {
         const results = createResults(itemListsToTest, daysToTest);
         const expectedResults = parseSavedResults(testPath);
 
-        expect(results.length).to.deep.equal(expectedResults.length);
-        expect(results[0].length).to.deep.equal(expectedResults[0].length);
-        expect(results[0][0].length).to.deep.equal(expectedResults[0][0].length);
+        expect(results.length).to.equal(expectedResults.length);
+        expect(results[0].length).to.equal(expectedResults[0].length);
+        expect(results[0][0].length).to.equal(expectedResults[0][0].length);
         expect(results).to.deep.equal(expectedResults);
     });
 });
